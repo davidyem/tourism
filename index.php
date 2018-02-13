ecef9a57e5c32caeceff4ad87c6881510252ebfe
Davyd <davidyeemelyanov@gmail.com>
2018-02-13 22:38:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico" tppabs="http://bootstrap-v4.ru/favicon.ico">
+
+    <title><?php echo $title; ?></title>
+
+    <!-- Подключение bootstrap CSS -->
+    <link href="http://91.218.195.182/dev/torusim/css/bootstrap.min.css" rel="stylesheet">
+    <link href="http://91.218.195.182/dev/torusim/css/style.css" rel="stylesheet">
+    <link href="http://91.218.195.182/dev/torusim/css/font-awesome.min.css" rel="stylesheet">
+    <link href="http://91.218.195.182/dev/torusim/css/jasny-bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Barlow Semi Condensed"/>
+    <link rel="stylesheet" href="http://ajax.aspnetcdn.com/ajax/jquery.ui/1.10.3/themes/sunny/jquery-ui.css">
+</head>
+<body>
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-default">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href="#"><img src="img/logo.png" class="logo"></a>
+                    </div>
+                    <ul class="nav navbar-nav">
+                        <li><a href="#"><span class="fa fa-instagram"></span> Instagram</a></li>
+                        <li><a href="tel:090 508 59 85"><span class="fa fa-phone"></span> 090 508 59 85</a></li>
+                        <li><a href="#"><span class="fa fa-envelope-o"></span> support@spiritairlines.com</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container">
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="col-lg-9 center-block" style="float: none">
+                <nav>
+                    <ul class="nav nav-pills nav-stacked span2 navbar-default">
+                        <li><a href="#">Новини</a></li>
+                        <li><a href="#">Пропозиції</a></li>
+                        <li><a href="#">Мій кабінет</a></li>
+                        <li><a href="#">Контакти</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <div class="col-lg-8">
+            <img src="img/3044_6.jpg">
+            <div class="col-lg-12 description">
+                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник
+                создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem
+                Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.
+                Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х
+                годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых
+                используется Lorem Ipsum.
+            </div>
+        </div>
+        <div class="col-lg-3"></div>
+        <div class="col-lg-8" style="margin-top: 20px;">
+            <img src="img/3044_6.jpg">
+            <div class="col-lg-12 description">
+                Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне. Lorem Ipsum является
+                стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий безымянный печатник
+                создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для распечатки образцов. Lorem
+                Ipsum не только успешно пережил без заметных изменений пять веков, но и перешагнул в электронный дизайн.
+                Его популяризации в новое время послужили публикация листов Letraset с образцами Lorem Ipsum в 60-х
+                годах и, в более недавнее время, программы электронной вёрстки типа Aldus PageMaker, в шаблонах которых
+                используется Lorem Ipsum.
+            </div>
+        </div>
+        <div class="col-lg-12 filters">
+            <div class="col-lg-3">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">За готелями
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">HTML</a></li>
+                        <li><a href="#">CSS</a></li>
+                        <li><a href="#">JavaScript</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">За датой
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">HTML</a></li>
+                        <li><a href="#">CSS</a></li>
+                        <li><a href="#">JavaScript</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Кількість осіб
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">HTML</a></li>
+                        <li><a href="#">CSS</a></li>
+                        <li><a href="#">JavaScript</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="dropdown">
+                    <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">За містами
+                        <span class="caret"></span></button>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">HTML</a></li>
+                        <li><a href="#">CSS</a></li>
+                        <li><a href="#">JavaScript</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3" style="margin-top: 10px;">
+            <img src="img/xudozhnik-Alexei-Butirskiy-01.jpg">
+            <h2>Lorem Ipsum</h2>
+        </div>
+        <div class="col-lg-3" style="margin-top: 10px;">
+            <img src="img/xudozhnik-Alexei-Butirskiy-01.jpg">
+            <h2>Lorem Ipsum</h2>
+        </div>
+        <div class="col-lg-3" style="margin-top: 10px;">
+            <img src="img/xudozhnik-Alexei-Butirskiy-01.jpg">
+            <h2>Lorem Ipsum</h2>
+        </div>
+        <div class="col-lg-3" style="margin-top: 10px;">
+            <img src="img/xudozhnik-Alexei-Butirskiy-01.jpg">
+            <h2>Lorem Ipsum</h2>
+        </div>
+        <div class="col-lg-12 footer">
+            <div class="col-lg-6">
+                <img src="img/logo.png" class="logo_footer"><br/>
+               <span class="address"> г. Киев, ул. Центральная, 158</span>
+            </div>
+            <div class="col-lg-6 copyright">
+                SpiritAIRlines ©
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script src="home_project/js/main.js"></script>
+<script type="text/javascript">
+    $(function () {
+        $('.sortable').sortable({
+            connectWith: '.sortable',
+            cancel: ".nodrag"
+        });
+    });
+</script>
+<script type="text/javascript" src="http://91.218.195.182/dev/torusim/js/bootstrap.min.js"></script>
+</body>
+</html>
