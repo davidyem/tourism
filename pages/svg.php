6995b71d786f6cb766761f0383dc9844fd994417
Davyd <davidyeemelyanov@gmail.com>
2018-03-26 21:36:40 +0300

    fifth commit

---
+++ pages/svg.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Svg</title>
+</head>
+<body>
+<svg width="400px" height="400px" >
+    <polygon points="80,160 98,106 160,106 110,70 130,12 80,48 30,12 50,70 0,106 62,106"
+             fill="#d66560" />
+    <circle r="30" cx="80" cy="80" fill="#d66560" stroke="white" stroke-width="3" />
+</svg>
+</body>
+</html>
